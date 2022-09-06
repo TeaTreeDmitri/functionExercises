38e35e01d5812c6cd4f07662e3fd5e3bdcf98eb0
Gareth Davies <mrgarethddavies@gmail.com>
2022-09-06 15:12:27 +0200

    capitalization and name-parts donzo

--- name-parts.js
+++ name-parts.js
@@ -1,11 +1,22 @@
-const oldString = "Gareth David Davies";
+// const oldString = "Gareth David Davies";
 
 
-splitString()
 
-function splitString() {
-    firstName = oldString.substring([0], oldString.firstIndexOf(" "));
-    middleName = oldString.substring(oldString.firstIndexOf(" "), oldString.lastIndexOf(" "));
-    lastName = oldString.substring(oldString.lastIndexOf(" "), );
-    console.log(firstName, middleName, lastName);
+// function splitString(fullName) {
+//     let firstName = fullName.substring([0], fullName.indexOf(" "));
+//     let middleName = fullName.substring(fullName.indexOf(" "), fullName.lastIndexOf(" "));
+//     let lastName = fullName.substring(fullName.lastIndexOf(" "), );
+//     return `My first name is ${firstName} and my second name is${middleName} and my last name is${lastName}`;
+// }
+
+// console.log(splitString("Gareth David Davies"))
+
+function fullName(lastName, firstName, middleName) {
+    if (middleName) {
+       // return `My first name is ${firstName} and my second name is ${middleName} and my last name is ${lastName}`;
+       return {firstName, middleName, lastName};
+    }
+        //return `My first name is ${firstName} and my last name is ${lastName}`;
+        return {firstName, lastName};
 }
+console.log(fullName("ingle","jeff", "superman"));
